d02fd9d31ebaa7c49d65b1720a71e0f528793c29
gycsaba <gycsaba@SZT9-24>
2024-09-26 16:22:57 +0200

    A kód

---
+++ SchoolClassManagmentProject/Models/Entities/SchoolClass.cs
@@ -0,0 +1,63 @@
+﻿using SchoolClassManagmentProject.Models.AppExceptions;
+
+namespace SchoolClassManagmentProject.Models.Entities
+{
+    public class SchoolClass
+    {
+        private byte _grade;
+        private char _gradeLetter;
+        private byte _lastGrade;
+        private int _classMoney;
+        public SchoolClass()
+        {
+            _grade = byte.MinValue;
+            _gradeLetter = char.MinValue;
+            _classMoney = int.MinValue;
+        }
+
+        public SchoolClass(byte grade, char gradeLetter, byte lastGrade)
+        {
+            _grade = grade;
+            _gradeLetter = gradeLetter;
+            _lastGrade = lastGrade;
+            _classMoney = int.MinValue;
+        }
+
+        public byte Grade { get => _grade; set => _grade = value; }
+        public char GradeLetter { get => _gradeLetter; set => _gradeLetter = value; }
+        public byte LastGrade { get => _lastGrade; private set => _lastGrade = value; }
+        public int ClassMoney { get => _classMoney; private set => _classMoney = value; }
+        public string Name => $"{_grade}. {_gradeLetter}";
+        public bool HasGraduated => _grade > _lastGrade;
+        public bool IsGraduate => _grade == _lastGrade;
+        public bool IsActive => !HasGraduated;
+
+        public void SetClassMoney(int classMoney)
+        {
+            _classMoney = classMoney; 
+        }
+        public void SetLastGrade(byte newGrade)
+        {
+            if (newGrade > _grade)
+                LastGrade = newGrade;
+            else
+                throw new LastGradeModificationErrorException($"{nameof(SchoolClass)} osztályba, {nameof(SetLastGrade)} metódusban paraméter hiba történt.", nameof(newGrade),null);
+        }
+
+        public void AdvanceGrade()
+        {
+            if (IsActive)
+                Grade = (byte) (Grade + 1);
+        }
+
+        ~SchoolClass()
+        {
+            Console.WriteLine($"{_grade}. {_gradeLetter} osztály megszünt.");
+        }
+
+        public override string ToString()
+        {
+            return $"{Grade}.{GradeLetter}";
+        }
+    }
+}
---
+++ SchoolClassManagmentProject/Repos/SchoolClassRepo.cs
@@ -0,0 +1,54 @@
+﻿using SchoolClassManagmentProject.Models.Entities;
+
+namespace SchoolClassManagmentProject.Repos
+{
+    public class SchoolClassRepo
+    {
+        private List<SchoolClass> _schoolClasses;
+        public SchoolClassRepo()
+        {
+            _schoolClasses = new List<SchoolClass>();
+        }
+        public int NumberOfSchoolClasses => _schoolClasses.Count;
+        public int NumberOfGraduateClasses => _schoolClasses.Count(schoolClass => schoolClass.IsGraduate);
+
+        public void Add(SchoolClass schoolClass)
+        {
+            _schoolClasses.Add(schoolClass);
+        }
+
+        public void Remove(int grade, char gradeLetter)
+        {
+            SchoolClass? schoolClassToRemove = _schoolClasses.Find(schoolClass => schoolClass.Grade == grade && schoolClass.GradeLetter == gradeLetter);
+            if (schoolClassToRemove is not null)
+            {
+                _schoolClasses.Remove(schoolClassToRemove);
+            }
+        }
+
+        public List<SchoolClass> GetSchoolClassesPerGrade(int grade)
+        {
+            return _schoolClasses.Where(schoolClass => schoolClass.Grade == grade).ToList();
+        }
+
+        public int GetNumberOfSchoolClassesPerGrade(int grade)
+        {
+            return GetSchoolClassesPerGrade(grade).Count;
+        }
+
+        public List<SchoolClass> GetGraduateClasses()
+        {
+            return _schoolClasses.Where(schoolClass => schoolClass.IsGraduate).ToList();
+        }
+
+        public List<byte> GetGrades()
+        {
+            return _schoolClasses.Select(schoolClass => schoolClass.Grade).Distinct().ToList();
+        }
+
+        public int GetClassMoneyPerStudentInOneYear(int grade, char gradeLetter)
+        {
+            return 10*_schoolClasses.Find(schoolClass => schoolClass.Grade == grade && schoolClass.GradeLetter == gradeLetter)?.ClassMoney ?? -1; // default(int);
+        }
+    }
+}
